a1dbdfc61104b13daf0da39f9ba50070c2a9c1f9
adyachenko <wolf.icefire@gmail.com>
2024-10-11 07:17:06 +0000

    fix

--- helpers/file_operations.py
+++ helpers/file_operations.py
@@ -26,7 +26,7 @@ def copy_files_only(source, dest):
 
 def scantree(path):
     """Recursively yield DirEntry objects for given directory."""
-    for entry in scandir(path):
+    for entry in os.scandir(path):
         if entry.is_dir(follow_symlinks=False):
             yield from scantree(entry.path)  # see below for Python 2.x
         else:
